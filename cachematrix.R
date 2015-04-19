0a64f32a9aec9983ea7f30c3bd25067b3e355bd7
dqfreed <dqfreed61@msn.com>
2015-04-19 07:48:11 -0400

    added comments to code

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## 19-Apr-2015 DQF
+## makeCacheMatrix store a square matrix in memory as  a R special matrix so the result is 
+## available for use in other R modules
+## sample commands to execute are below
+## create a square matrix daniel=matrix(c(2,78,90,45),nrow=2)
+##t=makeCacheMatrix(daniel)
+##cacheSolve(t)
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
@@ -15,7 +18,9 @@ getinverse <- function () inv
 list(set=set, get=get, setinv=setinverse, getinv=getinverse)
 }
 
-## Write a short comment describing this function
+## 19-Apr-2015 DQF 
+## cacheSolve takes in a square matrix and creates the inverse using solve.
+## if the inverse matrix exists the code displays a message indicating the cache version is being used
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -24,14 +29,9 @@ cacheSolve <- function(x, ...) {
     message("getting cached data")
     return(inv)
   }
-  ##mat.data = x$get()
-  ##print(mat.data)
-  ##inv = solve(mat.data,...)
+  
   n = x$get()
   inv = solve(n)
   x$setinv(inv)
   return (inv)
 }
-## create a square matrix daniel=matrix(c(2,78,90,45),nrow=2)
-##t=makeCacheMatrix(daniel)
-##cacheSolve(t)
